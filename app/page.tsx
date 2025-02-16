c30f1f88147685177c008d35521e39384e8493b7
Karim248119 <karimoda66@gmail.com>
2025-02-16 03:00:12 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Spline from "@splinetool/react-spline";
 export default function Home() {
   return (
     <main className="w-full h-screen">
-      <Spline scene="https://prod.spline.design/cVtwg880JECXupT1/scene.splinecode" />
+      <Spline scene="https://prod.spline.design/hwmHHfsbVRJhbWDQ/scene.splinecode" />
     </main>
   );
 }
